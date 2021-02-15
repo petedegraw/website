34b8cca68159d094c5228fccfc015539536cc93d
Pete DeGraw <2252478+petedegraw@users.noreply.github.com>
2021-02-15 03:12:21 +0000

    Updates all the things.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './styles.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+window.onload = () => document.querySelector('section').classList.add('active');
+let activeSection = 'home';
+let devSection = document.getElementById('dev');
+document.body.addEventListener('click', function (e) {
+    if (e.target && e.target.nodeName == 'A') {
+        e.preventDefault();
+        activeSection = e.target.href.split('#')[1];
+        setTimeout(function () {
+            document.querySelector('section.active').classList.remove('active');
+            document.getElementById(activeSection).classList.add('active');
+            if (Array.from(document.getElementById('dev').classList).includes('active')) {
+                Array.from(document.querySelectorAll('.highlights li')).forEach((el, index) => {
+                    let value = el.getAttribute('data-highlight');
+                    setTimeout(function () {
+                        el.style.width = Number(value) * 10 + '%';
+                        el.classList.add('animated');
+                    }, index * 100 + 100);
+                });
+            } else {
+                Array.from(document.querySelectorAll('.highlights li')).forEach((el, index) => {
+                    let value = el.getAttribute('data-highlight');
+                    setTimeout(function () {
+                        el.style.width = 40 + '%';
+                        el.classList.remove('animated');
+                    }, index * 100 + 100);
+                });
+            }
+        }, 300);
+    }
+});
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    plugins: [new MiniCssExtractPlugin()],
+};
+
+module.exports = config;
