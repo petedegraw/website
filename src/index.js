702b7e21205d58522f33762c5742e2f7ecb2422d
Pete DeGraw <pdegraw@equinix.com>
2021-02-20 21:25:02 +0000

    refactors script

--- src/index.js
+++ src/index.js
@@ -3,43 +3,37 @@ import './styles.scss';
 let activeSection = 'home';
 const devSection = document.getElementById('dev');
 
-function isDevSectionActive() {
-    return Array.from(document.getElementById('dev').classList).includes('active') ? true : false;
-}
+const isDevSectionActive = () =>
+    Array.from(devSection.classList).includes('active') ? true : false;
 
-function toggleListItemAnimation() {
+const toggleListItemAnimation = () => {
     const elToAnimate = '.highlights li';
-    const highlightedElementActive = Array.from(
-        document.querySelector(elToAnimate).classList
-    ).includes('animated');
+    const elToAnimateArr = Array.from(document.querySelectorAll(elToAnimate));
+    const isAnimatedElActive = Array.from(document.querySelector(elToAnimate).classList).includes(
+        'animated'
+    );
     if (isDevSectionActive()) {
-        Array.from(document.querySelectorAll(elToAnimate)).forEach((el, index) => {
+        elToAnimateArr.forEach((el, index) => {
             let value = el.getAttribute('data-highlight');
-            setTimeout(function () {
-                if (isDevSectionActive()) {
-                    el.style.width = Number(value) * 10 + '%';
-                    el.classList.add('animated');
-                } else {
-                    el.style.width = 40 + '%';
-                    el.classList.remove('animated');
-                }
+            setTimeout(() => {
+                el.style.width = Number(value) * 10 + '%';
+                el.classList.add('animated');
             }, index * 100 + 100);
         });
-    } else if (highlightedElementActive) {
-        console.log('we got one');
-        Array.from(document.querySelectorAll(elToAnimate)).forEach((el, index) => {
+    } else if (isAnimatedElActive) {
+        elToAnimateArr.forEach((el, index) => {
             let value = el.getAttribute('data-highlight');
             el.style.width = 40 + '%';
             el.classList.remove('animated');
         });
     }
-}
+};
 
-document.body.addEventListener('click', function (e) {
+document.body.addEventListener('click', (e) => {
     if (e.target && e.target.nodeName == 'A' && e.target.href.includes('#')) {
         e.preventDefault();
         activeSection = e.target.href.split('#')[1];
-        setTimeout(function () {
+        setTimeout(() => {
             document.querySelector('section.active').classList.remove('active');
             document.getElementById(activeSection).classList.add('active');
             toggleListItemAnimation();
