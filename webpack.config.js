c89859b750bcb3158024bcc8bef4d67783325623
Pete DeGraw <pdegraw@equinix.com>
2021-02-19 19:49:27 -0500

    adds server

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,29 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-            },
-        ],
-    },
-    plugins: [new MiniCssExtractPlugin()],
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			use: 'babel-loader',
+			exclude: /node_modules/,
+		}, {
+			test: /\.scss$/,
+			use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+		}, ],
+	},
+	plugins: [new MiniCssExtractPlugin()],
+	devServer: {
+		open: true,
+		publicPath: "",
+		contentBase: path.resolve(__dirname, "dist"),
+		watchContentBase: true,
+		compress: true,
+	},
 };
 
 module.exports = config;
