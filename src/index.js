13cdfb717fe5fc4911b9123471c28ae3e02ae2bc
Pete DeGraw <pdegraw@equinix.com>
2021-02-20 21:06:02 +0000

    Updates script functions.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import './styles.scss';
 
-window.onload = () => {
-    document.querySelector('body').classList.remove('no-js');
-    document.querySelector('section').classList.add('active');
-};
 let activeSection = 'home';
 let devSection = document.getElementById('dev');
+
+function isDevSectionActive() {
+    return Array.from(document.getElementById('dev').classList).includes('active') ? true : false;
+}
+
+function toggleListItemAnimation() {
+    const elToAnimate = '.highlights li';
+    const highlightedElementActive = Array.from(
+        document.querySelector(elToAnimate).classList
+    ).includes('animated');
+    if (isDevSectionActive()) {
+        Array.from(document.querySelectorAll(elToAnimate)).forEach((el, index) => {
+            let value = el.getAttribute('data-highlight');
+            setTimeout(function () {
+                if (isDevSectionActive()) {
+                    el.style.width = Number(value) * 10 + '%';
+                    el.classList.add('animated');
+                } else {
+                    el.style.width = 40 + '%';
+                    el.classList.remove('animated');
+                }
+            }, index * 100 + 100);
+        });
+    } else if (highlightedElementActive) {
+        console.log('we got one');
+        Array.from(document.querySelectorAll(elToAnimate)).forEach((el, index) => {
+            let value = el.getAttribute('data-highlight');
+            el.style.width = 40 + '%';
+            el.classList.remove('animated');
+        });
+    }
+}
+
 document.body.addEventListener('click', function (e) {
     if (e.target && e.target.nodeName == 'A' && e.target.href.includes('#')) {
         e.preventDefault();
@@ -13,23 +42,12 @@ document.body.addEventListener('click', function (e) {
         setTimeout(function () {
             document.querySelector('section.active').classList.remove('active');
             document.getElementById(activeSection).classList.add('active');
-            if (Array.from(document.getElementById('dev').classList).includes('active')) {
-                Array.from(document.querySelectorAll('.highlights li')).forEach((el, index) => {
-                    let value = el.getAttribute('data-highlight');
-                    setTimeout(function () {
-                        el.style.width = Number(value) * 10 + '%';
-                        el.classList.add('animated');
-                    }, index * 100 + 100);
-                });
-            } else {
-                Array.from(document.querySelectorAll('.highlights li')).forEach((el, index) => {
-                    let value = el.getAttribute('data-highlight');
-                    setTimeout(function () {
-                        el.style.width = 40 + '%';
-                        el.classList.remove('animated');
-                    }, index * 100 + 100);
-                });
-            }
+            toggleListItemAnimation();
         }, 300);
     }
 });
+
+window.onload = () => {
+    document.querySelector('body').classList.remove('no-js');
+    document.querySelector('section').classList.add('active');
+};
